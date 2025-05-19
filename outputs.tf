42209b618e63b54e7df608dfa5d38792cd3af690
NESTOR GONCALVES DE RESENDE JUNIOR <nestor.junior@gmail.com>
2025-05-19 11:56:43 -0300

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "vdi_instance_public_ip" {
+  description = "IP público da instância VDI."
+  value       = oci_core_instance.vdi_instance.public_ip
+}
+
+output "vdi_instance_ocid" {
+  description = "OCID da instância VDI."
+  value       = oci_core_instance.vdi_instance.id
+}
+
+output "ssh_command" {
+  description = "Comando para conectar via SSH à instância (substitua 'debian' pelo usuário apropriado se for diferente, e o caminho da chave se necessário)."
+  value       = "ssh debian@${oci_core_instance.vdi_instance.public_ip}"
+}
+
+output "vnc_connection_info" {
+  description = "Para conectar via VNC, use o IP público e a porta 5901 (ex: SEU_IP_PUBLICO:5901). A senha foi definida na variável 'vnc_password'."
+  value       = "${oci_core_instance.vdi_instance.public_ip}:5901"
+}
